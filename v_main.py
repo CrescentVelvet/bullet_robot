97d5d87cfd07b89e001fc9698f1a24f676372752
velvet <crescentvelvet3@gmail.com>
2021-03-27 18:50:32 +0800

    learn函数里添加两个循环

--- v_main.py
+++ v_main.py
@@ -7,9 +7,9 @@ import pleg.envs.environment
 env = gym.make('MyEnv-v0', render=True) # 建立可视化环境
 env = env.unwrapped # 解除环境限制
 model = PPO(env, verbose=1) # 建立PPO模型
-total_timesteps = 10000
-model.learn(total_timesteps=total_timesteps)
-model.save('v-wyf-pleg')
+total_timesteps = 10000 # 循环设定总步数
+model.learn(total_timesteps=total_timesteps) # 模型训练
+model.save('v-wyf-pleg') # 模型保存
 env = model.get_env()
 del model
 model = PPO.load(r'v-wyf-pleg.zip')
--- v_ppo.py
+++ v_ppo.py
@@ -1,6 +1,7 @@
 # 建立PPO模型
 import torch
 import datetime
+import numpy as np
 from torch.optim import Adam
 import pleg.envs.environment
 from v_network import FeedForwardNN
@@ -14,22 +15,25 @@ class PPO:
         self.act_std = 1 # 之后计算cov_mat协方差矩阵时需要用到的对角阵的主对角线值
         self.cov_var = torch.full(size=(self.act_dim,), fill_value=0.5)
         self.cov_mat = torch.diag_embed(self.cov_var).to(self.device)
-        self.actor  = FeedForwardNN(self.obs_dim, self.act_dim) # actor网络初始化
-        self.critic = FeedForwardNN(self.obs_dim, 1) # critic网络初始化
-        self.actor_optim = Adam(self.actor.parameters(), lr=2.5e-6) # 定义Adam优化器
-        self.critic_optim = Adam(self.critic.parameters(), lr=1e-4) # 定义Adam优化器
+        self.actor = FeedForwardNN(self.obs_dim, self.act_dim).to(self.device) # actor网络初始化
+        self.critic = FeedForwardNN(self.obs_dim, 1).to(self.device) # critic网络初始化
+        self.actor_optim = Adam(self.actor.parameters(), lr=self.actor_lr) # 定义actor模型的Adam优化器
+        self.critic_optim = Adam(self.critic.parameters(), lr=self.critic_lr) # 定义critic模型的Adam优化器
         self.now = datetime.datetime.now().strftime("%D-%H-%M").replace('/','-') # 当前时间
     def _init_hyperparameters(self, hyperparameters): # 定义默认的超参数
         self.timesteps_per_batch = 4800 # 运行一个batch的步数,batch是一次批处理的数据集样本数
         self.max_timesteps_per_episode = 1600 # 运行一个episode的最大步数,episode是agent在环境中根据某个策略执行一系列action到结束的过程
         self.n_updates_per_iteration = 5 # 每次迭代更新actor/critic的次数
-        self.lr = 0.005 # actor优化器的学习率
         self.gamma = 0.95 # 计算reward时的折扣系数
         self.clip = 0.2 # clip参数，建议为0.2
         self.render = False # 是否在rollout期间可视化
         self.save_freq = 10 # 迭代多少次保存一下
         self.seed = None # 设置程序种子,用于结果再现
-        self.lr = 0.005 # 设置Adam参数
+        self.actor_lr = 2.5e-6 # 设置actor模型的Adam学习率参数
+        self.critic_lr = 1e-4 # 设置critic模型的Adam学习率参数
+        self.epochs = 4
+        self.entropy_beta = 0.1
+        self.minibatch_size = 256
         for param, val in hyperparameters.items(): # 将超参数的默认值修改为自定义值
             exec('self.' + param + ' = ' + str(val))
         if self.seed != None: # 设置程序种子
@@ -39,21 +43,31 @@ class PPO:
         t_so_far = 0 # 到目前为止已经训练了多少步
         while t_so_far < total_timesteps: # 循环设定总步数
             batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens = self.rollout() # 运行策略收集数据batch
-            V, curr_log_probs = self.evaluate(batch_obs, batch_acts) # 计算obs的预测值（V-value）
-            A_k = batch_rtgs - V.detach() # 计算优势函数
-            A_k = (A_k - A_k.mean()) / (A_k.std() + 1e-10) # 对优势函数进行规范化
-            ratios = torch.exp(curr_log_probs - batch_log_probs) # 两个log概率相减再取e的幂
-            surr1 = ratios * A_k # 计算代替损失
-            surr2 = torch.clamp(ratios, 1 - self.clip, 1 + self.clip) * A_k
-            actor_loss = (-torch.min(surr1, surr2)).mean() # 计算loss
-            self.actor_optim.zero_grad() # 计算梯度
-            actor_loss.backward() # 反向传播
-            self.actor_optim.step() # actor网络执行一步
-            self.critic_optim = Adam(self.critic.parameters(), lr=self.lr) # 定义critic的Adam优化器
-            critic_loss = torch.nn.MSELoss()(V, batch_rtgs) # 计算loss
-            self.critic_optim.zero_grad() # 计算梯度
-            critic_loss.backward() # 反向传播
-            self.critic_optim.step() # critic网络执行一步
+            t_so_far += np.sum(batch_lens) # 循环步数加上一个batch的步数
+            for i in range(self.epochs):
+                for k in range(0, batch_rtgs.shape[0], self.minibatch_size):
+                    obs_state = batch_obs[k : k + self.minibatch_size]
+                    acts = batch_acts[k : k + self.minibatch_size]
+                    rtgs = batch_rtgs[k : k + self.minibatch_size]
+                    log_probs = batch_log_probs[k : k + self.minibatch_size]
+                    # V, curr_log_probs = self.evaluate(batch_obs, batch_acts) # 计算obs的预测值（V-value）
+                    V, _ = self.evaluate(obs_state, acts) # 计算obs的预测值（V-value）
+                    A_k = rtgs - V.detach() # 计算优势函数
+                    del V
+                    A_k = (A_k - A_k.mean()) / (A_k.std() + 1e-10) # 对优势函数进行规范化
+                    V, curr_log_probs = self.evaluate(obs_state, acts) # 计算obs的预测值（V-value）
+                    ratios = torch.exp(curr_log_probs - log_probs) # 两个log概率相减再取e的幂
+                    surr1 = ratios * A_k # 计算代替损失
+                    surr2 = torch.clamp(ratios, 1 - self.clip, 1 + self.clip) * A_k
+                    entropy = self.entropy_beta * (-(torch.exp(curr_log_probs) * curr_log_probs)).mean()
+                    actor_loss = (-torch.min(surr1, surr2)).mean() - entropy # 计算loss
+                    self.actor_optim.zero_grad() # 计算梯度
+                    actor_loss.backward() # 反向传播
+                    self.actor_optim.step() # actor网络执行一步
+                    critic_loss = torch.nn.MSELoss()(V, rtgs) # 计算loss
+                    self.critic_optim.zero_grad() # 计算梯度
+                    critic_loss.backward() # 反向传播
+                    self.critic_optim.step() # critic网络执行一步
     def rollout(self): # rollout就是运行当前actor策略并从一组episode中收集数据batch
         batch_obs = [] # batch观测空间,维度(一个batch步数,观测空间维度)
         batch_acts = [] # batch动作空间,维度(一个batch步数,动作空间维度)
