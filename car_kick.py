ffcf91cf40ce921cf5a464fca325862e6b4b5b11
velvet <crescentvelvet3@gmail.com>
2021-04-12 21:46:59 +0800

    自动更新ｉｎｉ文件参数

--- car_kick.py
+++ car_kick.py
@@ -1,31 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import configparser
+# encoding="utf-8"
 class car_data: # 单个车数据
     def __init__(self):
         self.id = -1
         self.maxvel = []
         self.power = []
         self.val_fit = []
-    def assign(self, id, maxvel, power):
+    def assign(self, id, maxvel, power): # 参数赋值
         self.id = id
         self.maxvel.append(maxvel)
         self.power.append(power)
-    def calculate(self):
+    def calculate(self): # 计算拟合函数
         if self.id == -1:
             self.val_fit = 0
         else:
             raw_fit = np.polyfit(self.maxvel, self.power, 2)
             self.val_fit = np.poly1d(raw_fit)
-    def draw(self):
-        plot_maxvel = np.arange(0, 7500, 1)
-        plot_power = self.val_fit(plot_maxvel)
-        plot1 = plt.plot(self.maxvel, self.power, '*', label='original values')
-        plot2 = plt.plot(plot_maxvel, plot_power, 'r', label='polyfit values')
+    def draw(self): # 绘制拟合曲线
+        if self.id == -1:
+            plot_maxvel = np.arange(0, 7500, 1)
+            plot_power = np.zeros(len(plot_maxvel))
+            plot2 = plt.plot(plot_maxvel, plot_power, 'r', label='polyfit values')
+        else:
+            plot_maxvel = np.arange(0, 7500, 1)
+            plot_power = self.val_fit(plot_maxvel)
+            plot1 = plt.plot(self.maxvel, self.power, '*', label='original values')
+            plot2 = plt.plot(plot_maxvel, plot_power, 'r', label='polyfit values')
         plt.xlabel('maxvel')
         plt.ylabel('power')
         plt.show()
-
 class car_cal: # 全部车数据
     def __init__(self):
         self.raw_id = []
@@ -36,12 +41,12 @@ class car_cal: # 全部车数据
         self.all_vel = []
         self.all_maxvel = []
         self.all_power = []
-    def read_data(self, address):
+    def read_data(self, address): # txt数据读取
         with open(address, "r") as f:
             raw_data = f.readlines()
             for line in raw_data:
                 line_data = line.split()
-                if line_data[1] == '0' or line_data[2] == '-1000':
+                if line_data[1] == '0' or line_data[2] == '-1000': # 排除异常数据
                     continue
                 if line_data[0] != '\n':
                     self.raw_id.append(line_data[0].strip())
@@ -51,8 +56,6 @@ class car_cal: # 全部车数据
                     self.raw_maxvel.append(line_data[2].strip())
                 if line_data[3] != '\n':
                     self.raw_power.append(line_data[3].strip())
-                if line_data[0] == '0':
-                    print(line_data[2])
         self.raw_id = list(map(float, self.raw_id))
         self.raw_vel = list(map(float, self.raw_vel))
         self.raw_maxvel = list(map(float, self.raw_maxvel))
@@ -61,22 +64,29 @@ class car_cal: # 全部车数据
         for i in range(len(self.raw_id)):
             if old_id != self.raw_id[i]:
                 old_id = self.raw_id[i]
-                print(self.raw_id[i-1], self.raw_vel[i-1], self.raw_maxvel[i-1], self.raw_power[i-1])
-                if self.raw_power[i-1] > 0.0 and self.raw_power[i-1] < 130.0 and self.raw_maxvel[i-1] > 0.0 and self.raw_maxvel[i-1] < 8000.0:
+                if self.raw_power[i-1] > 0.0 and self.raw_power[i-1] < 130.0 and self.raw_maxvel[i-1] > 0.0 and self.raw_maxvel[i-1] < 8000.0: # 排除超量数据
                     self.all_id.append(self.raw_id[i-1])
                     self.all_vel.append(self.raw_vel[i-1])
                     self.all_maxvel.append(self.raw_maxvel[i-1])
                     self.all_power.append(self.raw_power[i-1])
-address = "/home/zjunlict-vision-1/Desktop/bullet_robot/VelData.txt"
-all_car = car_cal() # 数据读取
-all_car.read_data(address)
+static_car_num = 16
+txt_address = "/home/zjunlict-vision-1/Desktop/dhz/Kun2/ZBin/data/VelData_6_10.txt"
+ini_address = "/home/zjunlict-vision-1/Desktop/dhz/Kun2/ZBin/kickparam.ini"
+car_all = car_cal() # txt数据读取
+car_all.read_data(txt_address)
 car_list = []
-for i in range(16): # 初始化
+for i in range(static_car_num): # 初始化
     car_list.append(car_data())
-for i in range(len(all_car.all_id)): # 车号划分
-    car_list[int(all_car.all_id[i])].assign(int(all_car.all_id[i]), all_car.all_maxvel[i], all_car.all_power[i])
-for i in range(16):
+for i in range(len(car_all.all_id)): # 车号划分
+    car_list[int(car_all.all_id[i])].assign(int(car_all.all_id[i]), car_all.all_maxvel[i], car_all.all_power[i])
+for i in range(16): # 计算拟合函数
     car_list[i].calculate()
-    print(car_list[i].id, '---', car_list[i].val_fit)
-print(all_car.all_id)
-car_list[0].draw()
+# car_list[10].draw()
+robot_conf = configparser.ConfigParser() # 创建管理对象
+robot_conf.read(ini_address, encoding="utf-8") # ini数据读取
+for robot_id in range(static_car_num):
+    if car_list[robot_id].id != -1: # ini参数更新
+        robot_conf.set("Robot"+str(robot_id), "flat_a", str(car_list[robot_id].val_fit[2]))
+        robot_conf.set("Robot"+str(robot_id), "flat_b", str(car_list[robot_id].val_fit[1]))
+        robot_conf.set("Robot"+str(robot_id), "flat_c", str(car_list[robot_id].val_fit[0]))
+robot_conf.write(open(ini_address, "w", encoding="utf-8"))
