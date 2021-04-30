2e98bc6154d42a7e99d4ca3e4252568138e095d4
velvet <crescentvelvet3@gmail.com>
2021-04-30 22:37:08 +0800

    加入测试单次踢球代码

--- car_chip.py
+++ car_chip.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
-class BallDataOne:
+class BallDataOne: # 单次踢球的球数据
     def __init__(self):
         self.id = -1
         self.vel = []
         self.dist = []
         self.power = []
         self.avg_vel = []
+        self.chip_point = -1
     def assign(self, id, vel, dist, power): # 数据赋值
         self.id = id
         self.vel.append(vel)
@@ -17,8 +18,9 @@ class BallDataOne:
         self.dist = []
         self.power = []
         self.avg_vel = []
-    def mean_filter(self, per): # 均值滤波
-        for i in range(0, len(self.vel)-per, per):
+        self.chip_point = -1
+    def analy(self, per):
+        for i in range(0, len(self.vel)-per, per): # 均值滤波
             sum = 0
             for j in range(per):
                 sum += self.vel[i+j]
@@ -26,33 +28,64 @@ class BallDataOne:
                 self.avg_vel.append(sum/per)
         max_vel = max(self.avg_vel) # 求最值
         max_index = self.avg_vel.index(max_vel)
-        chip_point = -1 #　找落地点
-        for i in range(max_index, len(self.avg_vel)-1):
+        self.chip_point = -1
+        for i in range(max_index, len(self.avg_vel)-1): #　找落地点
             if self.avg_vel[i] < self.avg_vel[i+1]:
-                chip_point = i
+                self.chip_point = i
                 break
-        return chip_point
-class BallDataAll:
+    def draw(self):
+        plt.plot(self.vel, color='limegreen')
+        plt.scatter(self.chip_point, self.vel[self.chip_point], s=500, color='deeppink', alpha=0.2)
+        plt.scatter(self.chip_point, self.vel[self.chip_point], s=200, color='deeppink', alpha=0.4)
+        plt.scatter(self.chip_point, self.vel[self.chip_point], s=50, color='mediumvioletred', alpha=1.0)
+        plt.plot(self.avg_vel, color='orangered')
+        plt.show()
+class BallTest:
+    def read(address):
+        ball = BallDataOne()
+        with open(address, "r") as f:
+            raw_data = f.readlines()
+            for line in raw_data:
+                line_data = line.split()
+                if line_data[1] != '\n' or line_data[1] != '0':
+                    ball.vel.append(line_data[1].strip())
+        ball.vel = list(map(float, ball.vel))
+        ball.analy(5)
+        ball.draw()
+        ball.clear()
+class BallDataAll: # 多次踢球的球数据
     def __init__(self):
         self.id = []
-        self.vel = []
-        self.dist = []
+        self.raw_vel = []
+        self.raw_dist = []
         self.power = []
-    def read_data(self, address): # 数据读取
+        self.real_dist = []
+    def read(self, address): # 数据读取
         with open(address, "r") as f:
             raw_data = f.readlines()
             for line in raw_data:
                 line_data = line.split()
                 if line_data[1] != '\n' or line_data[1] != '0':
                     self.id.append(line_data[0].strip())
-                    self.vel.append(line_data[1].strip())
-                    self.dist.append(line_data[2].strip())
+                    self.raw_vel.append(line_data[1].strip())
+                    self.raw_dist.append(line_data[2].strip())
                     self.power.append(line_data[3].strip())
         self.id = list(map(int, self.id))
-        self.vel = list(map(float, self.vel))
-        self.dist = list(map(float, self.dist))
+        self.raw_vel = list(map(float, self.raw_vel))
+        self.raw_dist = list(map(float, self.raw_dist))
         self.power = list(map(float, self.power))
-class CarData:
+        old_id = self.id[0]
+        ball = BallDataOne()
+        for i in range(self.id):
+            if old_id == self.id[i]: # id不变时计算落地点
+                ball.assign(self.id[i], self.vel[i], self.dist[i], self.power[i])
+            else: # id变化时记录距离和力度
+                old_id= self.id[i]
+                ball.analy(5)
+                if ball.chip_point != -1:
+                    self.real_dist.append(ball.dist[ball.chip_point])
+                ball.clear()
+class CarDataOne:
     def __init__(self):
         self.id = -1
         self.dist = []
@@ -79,36 +112,7 @@ class CarData:
         plt.xlabel('dist')
         plt.ylabel('power')
         plt.show()
+# class CarDataAll:
 # class ChipData:
-txt_address = "/home/zjunlict-vision-1/Desktop/dhz/Kun2/ZBin/data/BallVel1.txt"
-raw_id = []
-raw_vel = []
-raw_dist = []
-avg_vel = []
-with open(txt_address, "r") as f: # 读取数据
-    raw_data = f.readlines()
-    for line in raw_data:
-        line_data = line.split()
-        if line_data[1] != '\n' or line_data[1] != '0':
-            raw_vel.append(line_data[1].strip())
-raw_vel = list(map(float, raw_vel))
-per = 5 # 均值滤波
-for i in range(0, len(raw_vel)-per, per):
-    sum = 0
-    for j in range(per):
-        sum += raw_vel[i+j]
-    for j in range(per):
-        avg_vel.append(sum/per)
-max_vel = max(avg_vel) # 求最值
-max_index = avg_vel.index(max_vel)
-chip_point = -1 #　找落地点
-for i in range(max_index, len(avg_vel)-1):
-    if avg_vel[i] < avg_vel[i+1]:
-        chip_point = i
-        break
-plt.plot(raw_vel, color='limegreen')
-plt.scatter(chip_point, raw_vel[chip_point], s=500, color='deeppink', alpha=0.2)
-plt.scatter(chip_point, raw_vel[chip_point], s=200, color='deeppink', alpha=0.4)
-plt.scatter(chip_point, raw_vel[chip_point], s=50, color='mediumvioletred', alpha=1.0)
-plt.plot(avg_vel, color='orangered')
-plt.show()
+# txt_address = "/home/zjunlict-vision-1/Desktop/dhz/Kun2/ZBin/data/BallVel1.txt"
+# BallTest.read(txt_address)
