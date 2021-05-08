0a1591cb19533a4dc800ffa3a4b8111d17fd7637
velvet <crescentvelvet3@gmail.com>
2021-05-08 21:53:51 +0800

    采集数据

--- car_vel/RegulationData.lua
+++ car_vel/RegulationData.lua
@@ -2,9 +2,11 @@ local PLACE_POS = CGeoPoint:new_local(0,0)
 local SHOOT_POS = CGeoPoint:new_local(4000,0)
 local TURN_DIR = -math.pi
 
-local rot_speed = 0
-local rot_limit = 0
+local rot_speed = 0.0
+local rot_limit = 1.0
 local power = 3000
+local kick_x = 0.0
+local kick_y = 0.0
 
 function getpower()
     return function()
@@ -24,13 +26,15 @@ function linedir()
 end
 function runpos()
     return function()
-        return PLACE_POS+CVector(90,0)
+        return PLACE_POS+CVector(-90,0)
     end
 end
 
 function getflag()
     return function()
-        if bufcnt(player.rotVel("Leader") > rot_limit - 0.08, 20) and math.abs(player.toPointDir(SHOOT_POS, "Leader")-player.dir("Leader"))<2*math.pi/180 then
+        if bufcnt(player.rotVel("Leader") > rot_limit - 0.08, 20) and math.abs(player.toPointDir(SHOOT_POS, "Leader")-player.dir("Leader"))<rot_limit/75.0 then
+            kick_x = player.posX("Leader")
+            kick_y = player.posY("Leader")
             return flag.dribble + flag.kick
         end
         return flag.dribble
@@ -40,14 +44,18 @@ end
 local process = function(power)
     local recordfile = io.open("data/regulation.txt","a")
     omega = rot_limit
-    debugEngine:gui_debug_msg(ball.pos(),"giao!!")
     delta_dir = (SHOOT_POS - PLACE_POS):dir() - (ball.pos() - PLACE_POS):dir()
     print(delta_dir)
     need_reg = power * math.tan(delta_dir)
     recordfile:write(omega," ",need_reg,"\n")
 end
 
-
+local savedata = function (rotvel, power)
+    debugEngine:gui_debug_msg(ball.pos(),"savedata!!!")
+    local recordfile = io.open("data/ReguData.txt", "a")
+    print(rotvel, power, ball.posY() - kick_y)
+    recordfile:write(" ",rotvel," ",power," ",ball.posX()-kick_x," ",ball.posY()-kick_y,"\n")
+end
 
 gPlayTable.CreatePlay{
 
@@ -72,8 +80,8 @@ firstState = "fetchBall",
         else
             if player.toTargetDist("Leader")< 200 then
                 if ball.toPointDist(PLACE_POS) < 200 and player.infraredOn("Leader") then
-                    rot_limit = rot_limit + 2.0
-                    if(rot_limit > 12.0) then
+                    rot_limit = rot_limit + 1.0
+                    if(rot_limit > 10.0) then
                         power = power + 500
                         rot_limit = 2.0
                     end
@@ -98,13 +106,11 @@ firstState = "fetchBall",
         else
             rot_speed = rot_speed + 0.08
         end
-        -- if math.abs(player.dir("Leader") - player.toPointDir(SHOOT_POS, "Leader")) < 10 / 180 * math.pi then
-        --     return "kick"
-        -- end
-        -- print("rot_speed",rot_speed)
         if bufcnt(not player.infraredOn("Leader"),9999) then
-            -- PLACE_POS = CGeoPoint:new_local((math.random() - 0.5) * 1000, (math.random() - 0.5) * 5000)
             return "fetchBall"
+        end
+        if player.kickBall("Leader") then
+            return "record"
         end
     end,
     Leader = task.openSpeed(0, 0, rotspeed(), 0, getflag(), SHOOT_POS, getpower()),
@@ -112,24 +118,14 @@ firstState = "fetchBall",
 },
 ["record"] = {
     switch = function()
-        if ball.toPointDist(PLACE_POS) > 2000 then
-            process((SHOOT_POS - PLACE_POS):mod() + 1000)
-            -- PLACE_POS = CGeoPoint:new_local((math.random() - 0.5) * 1000, (math.random() - 0.5) * 5000)
+        if (ball.posX()-kick_x) > 1000 then
+            -- process((SHOOT_POS - PLACE_POS):mod() + 1000)
+            savedata(rot_limit, power)
             return "fetchBall"
         end
     end,
     match = ""
 },
--- ["kick"] = {
---     switch = function()
---         if bufcnt(player.kickBall("Leader"), 1 ,300) then
---             return "fetchBall"
---         end
---     end,
---     Leader = task.zget(SHOOT_POS, _, _, flag.kick),
---     -- Leader = task.zbreak(SHOOT_POS, _, _, _),
---     match = "{L}"
--- },
 
 ["waitforball"] = {
     switch = function()
